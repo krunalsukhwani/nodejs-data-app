ef8bb5ce11e12655f23c4fd2da4a2262314c3466
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-09-19 21:25:32 -0400

    Handler GET and Post request
    installed body-parser library
    used this library to get username

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 //how to import the library
 const express = require('express');
+const bodyParser = require('body-parser');
 
 //create the server
 const app = express();
 
-//localhost:8080
+//use body parser to parse data coming from UI
+app.use(bodyParser.urlencoded({extended: false}));
+
+//localhost:8080 - GET - Display form
 app.get('/', (req, res, next) => {
     return res.send('<h1>Welcome to My Application</h1>'+
-    '<form method="GET" action="/chris"><button type="submit">Chris</button></form>' +
-    '<form method="GET" action="/omair"><button type="submit">Omair</button></form>');
+    '<form method="POST" action="/user"><input type="text" name="username"><button type="submit">Create USER</button></form>');
 });
 
-//Middleware can manage get request: path
-app.get('/chris', (req, res, next) => {
-    return res.send('<h1>Chris</h1>');
-});
-
-app.get('/omair', (req, res, next) => {
-    return res.send('<h1>Omair Khan</h1>');
+//POST request with path: /user - display username
+app.post('/user', (req, res, next) => {
+    return res.send('<h1> Username: ' + req.body.username + '</h1>')
 });
 
 //on which port you want to start the server
